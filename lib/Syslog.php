024d8ab4a462b9e45ac2eb65c575344e99f1f246
Kaj-Michael Lang <milang@tal.org>
2017-02-16 22:49:11 +0200

    Add optional Exception argument for logging

--- lib/Syslog.php
+++ lib/Syslog.php
@@ -1,12 +1,15 @@
 <?php
 
-function slog($er, $data='')
+function slog($er, $data='', Exception $e=null)
 {
 openlog("rvapi", LOG_PID, LOG_LOCAL0);
 if (!is_string($data))
 	$data=json_encode($data);
 syslog(LOG_WARNING, "$er {$_SERVER['REMOTE_ADDR']} ({$_SERVER['HTTP_USER_AGENT']})");
-syslog(LOG_DEBUG, $data);
+if ($data)
+	syslog(LOG_WARNING, $data);
+if (!is_null($e))
+	syslog(LOG_WARNING, sprintf('Exception [%d]: %s', $e->getCode(), $e->getMessage()));
 closelog();
 }
 

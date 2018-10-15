85feaeaf4daebb0fe29a2913873a239b84124c5e
Kaj-Michael Lang <milang@tal.org>
2018-10-15 10:09:29 +0300

    Optional traceback in log

--- lib/Syslog.php
+++ lib/Syslog.php
@@ -1,6 +1,6 @@
 <?php
 // XXX We would like to have Throwable/Exception $e but we need to be PHP 5 compatible so for now...
-function slog($er, $data='', $e=null)
+function slog($er, $data='', $e=null, $trace=false)
 {
 openlog("rvapi", LOG_PID, LOG_LOCAL0);
 if (!is_string($data))
@@ -8,8 +8,11 @@ if (!is_string($data))
 syslog(LOG_WARNING, "$er {$_SERVER['REMOTE_ADDR']} ({$_SERVER['HTTP_USER_AGENT']})");
 if ($data)
 	syslog(LOG_WARNING, $data);
-if (!is_null($e))
+if (!is_null($e)) {
 	syslog(LOG_WARNING, sprintf('Exception [%d] [%d in %s]: %s', $e->getCode(), $e->getLine(), $e->getFile(), $e->getMessage()));
+	if ($trace)
+		syslog(LOG_DEBUG, json_encode($e->getTrace()));
+}
 closelog();
 }
 

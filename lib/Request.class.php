83abec26b121a59e9591e4b3a9e25a85d1ff06a9
Kaj-Michael Lang <milang@tal.org>
2018-10-25 12:59:16 +0300

    Fix it

--- lib/Request.class.php
+++ lib/Request.class.php
@@ -4,20 +4,19 @@ class Request
 {
 private $d;
 
-public __construct(array $data)
+public function __construct($data)
 {
 $this->d=$data;
 }
 
-public getStr($key, $def=null)
+public function getStr($key, $def=null)
 {
 if (!isset($this->d[$key]))
 	return $def;
-
 return filter_var($this->d[$key], FILTER_SANITIZE_STRING);
 }
 
-public getInt($key, $def=null)
+public function getInt($key, $def=null)
 {
 if (!isset($this->d[$key]))
 	return $def;

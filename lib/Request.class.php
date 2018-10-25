1f83fe9ae631d70420a02c0b3706e5e11d46e2ff
Kaj-Michael Lang <milang@tal.org>
2018-10-25 16:06:32 +0300

    Add one more request helper

--- lib/Request.class.php
+++ lib/Request.class.php
@@ -16,6 +16,18 @@ if (!isset($this->d[$key]))
 return filter_var($this->d[$key], FILTER_SANITIZE_STRING);
 }
 
+public function getStrValidate($key, array $valid, $def=null, $defv=null)
+{
+$s=$this->getStr($key, false);
+if ($s===false)
+	return $def;
+
+if (!in_array($s, $valid, true))
+	return $defv;
+
+return $s;
+}
+
 public function getInt($key, $def=null)
 {
 if (!isset($this->d[$key]))

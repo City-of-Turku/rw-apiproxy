a977c392a912ae76ce7781e6d3a98abaf37a3502
Kaj-Michael Lang <milang@tal.org>
2018-11-21 14:02:37 +0200

    Cleanup response

--- lib/Response.class.php
+++ lib/Response.class.php
@@ -15,28 +15,23 @@ class ResponseException extends Exception {}
 class Response
 {
 
-
 public static function data($code, $message, $op, array $data=null)
 {
-$meta=array('code'=>$code, 'message'=>$message, 'op'=>$op);
 return array(
 	'version'=>1,
 	'code'=>$code,
 	'message'=>$message,
 	'op'=>$op,
-	'meta'=>$meta,
 	'data'=>$data);
 }
 
 public static function error($code, $error, $op)
 {
-$meta=array('code'=>$code, 'error'=>$error, 'op'=>$op);
 return array(
 	'version'=>1,
 	'code'=>$code,
 	'error'=>$error,
-	'op'=>$op,
-	'meta'=>$meta);
+	'op'=>$op);
 }
 
 } // class
